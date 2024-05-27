75ffc76fa8a866deae6858f38f73c0b3e14f0127
tanthehack <31630356+tanthehack@users.noreply.github.com>
2024-05-27 22:15:34 +0100

    add: landing layout

--- src/layouts/home.jsx
+++ src/layouts/home.jsx
@@ -1,9 +1,25 @@
 import { Outlet } from "react-router-dom"
+import bgLight from '../assets/images/homeBg1.svg'
+import bgDark from '../assets/images/homeBg2.svg'
+
+import grdLight from '../assets/images/bgGrad1.svg'
+import grdDark from '../assets/images/bgGrad2.svg'
+
+import logo from '../assets/images/logo.svg'
 
 export const Home = () => {
     return (
-        <main className="h-dvh font-main dark:bg-coal-main bg-gray-main dark:text-white text-coal-main">
-            <Outlet />
+        <main className="relative h-dvh font-main dark:bg-coal-main bg-gray-main dark:text-white text-coal-main">
+            <img src={bgLight} alt="background elements showing an @ symbol and *c symbol" className="absolute h-full w-full dark:hidden" />
+            <img src={bgDark} alt="background elements showing an @ symbol and *c symbol" className="dark:absolute dark:block h-full w-full hidden" />
+            <img src={grdLight} alt="an orange gradient" className="absolute h-full w-full dark:hidden z-[0]" />
+            <img src={grdDark} alt="an orange gradient" className="dark:absolute dark:block h-full w-full hidden z-[0]" />
+            <div className="w-full flex justify-center pt-8">
+                <img src={logo} alt="Re-code logo in orange" />
+            </div>
+            <div className="z-[1] w-full">
+                <Outlet />
+            </div>
         </main>
     )
 } 
